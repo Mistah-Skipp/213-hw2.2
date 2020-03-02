e8391305edecbc397f81f311c3204e30bfb0cc3a
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2020-03-02 17:05:08 -0500

    p3 update
    copied new shit over, basically all that is left is read infile and copy to gameboard via col/3 and row%3 (possible swap row and col)

--- problem3.py
+++ problem3.py
@@ -1,32 +1,69 @@
 # hw 2.3. create a program to display a 2D list formation on screen for each each tictactoe end game saved in a given text file where each end game is represented in a line of one-d array.
+tttable = None
+
+def createboard():
+    global tttable
+    tttable = list()
+    for i in range (4):
+        row = list()
+        for t in range (4): 
+            row.append(t)
+            tttable.append(row)
+
+    print("____________________")
+#createboard_End
+
+def displayboard():
+    for r in range(0,4):
+        print()
+        for c in range(0,4):
+            print(tttable[r][c], end = " ")
+    print()
+
+    print("____________________")
+#displayboard_End
+def fillboard():
+    global tttable
+    for c in range (4):
+        #templist to store games
+        tttable [0][c] = masterList[c]
+
+#fillTable_End
 
 
 #Entry_Point
-
-#1dboard.txt is a arbitrary file that contains a string of the tic-tac-toe games
+createboard()
+displayboard()
 ttt = open("test.txt","r")
 #print (ttt.read() )
 #make 2d list, write each game into the list and save to endgame.txt
-tttEnd = open("endgame.txt", "w")
-temp = list(ttt.read())
-tttable = list()
-for i in range (3):
-    row = list()
-    for t in range (3): 
-        row.append(t)
-        tttable.append(row)
+tttEnd = open("endgame.txt", "w")#makes endgame.txt and opens writable
+masterList = list(ttt.read())
+print(masterList, ' ')
+displayboard()
 
-print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-pos = 0
-for r in range (3):
-    for c in range (3):
-        if (temp[pos] == ' '):
-            print ("S")
-        else:
-            tttable [r][c] = temp[pos]
-        pos+=1
-        print ( tttable[r][c], end = '' )
+print("G1:")
+createboard()
+fillboard()
+displayboard()
 
+print("G2:")
+createboard()
+fillboard()
+displayboard()
+
+
+'''
+def loadGame(gameid):   adapt to 3x3
+    load = open(gameid, 'r')
+    row = 0
+    for rank in load:
+        for col in range (8):
+            chessboard[row][col] = rank[col]
+        row+=1
+        if row == 8:
+            break
+            '''
 
 '''
 temp = ( ttt.read() )
